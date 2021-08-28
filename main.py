56198e5a96d09867bd07cc9684653a2517904b8e
jonah <jonah.d.temple@gmail.com>
2021-08-28 18:33:07 -0400

    Minor formatting fixes

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import random
 from typing import List, Dict
 from statistics import mean, median
 from collections import Counter
+
 
 class Card:
     def __init__(self, suit, value, owner):
